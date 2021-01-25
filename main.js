5f28e7afce2739bd46bb13f3fd2484f802ff4e64
anabrd <ana.d.brdar@gmail.com>
2021-01-25 08:59:27 +0100

    fix last frame

--- main.js
+++ main.js
@@ -7,22 +7,24 @@ function roll() {
     let rotationAngle = 45;
     let framesUp = 6;
     let marginTop = 1;
-    let diceNum = Math.floor(Math.random()*6 + 1);
+    let diceNum = Math.floor(Math.random() * 6 + 1);
 
     function animate() {
 
-        if (framesUp > 0) {
+        if (framesUp > 1) {
             dice.src = `./assets/dice${framesUp}.png`
             dice.style["margin-top"] = `${marginTop}%`;
             dice.style.transform = `rotate(${rotationAngle}deg)`;
             framesUp--;
-            rotationAngle*=2;
-            marginTop*=2;
+            rotationAngle *= 2;
+            marginTop *= 2;
+        } else if (framesUp == 1) {
+            dice.src = `./assets/dice${diceNum}.png`
+            dice.style["margin-top"] = `${marginTop}%`;
+            framesUp--;
         } else {
-        dice.src = `./assets/dice${diceNum}.png`
-        clearInterval(animation);
+            clearInterval(animation);
         }
-
     }
 
     document.getElementById("p2").innerHTML = "You rolled: " + diceNum;
